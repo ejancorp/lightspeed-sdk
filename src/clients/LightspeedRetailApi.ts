831905dbb72c6a9f7e0a6f2d6eca2eb1fe950dc0
Efren Jan Corpuz <ejan.corp@gmail.com>
2024-09-27 07:37:54 +0400

    Update LightspeedRetailApi.ts

--- src/clients/LightspeedRetailApi.ts
+++ src/clients/LightspeedRetailApi.ts
@@ -472,6 +472,23 @@ class LightspeedRetailApi {
     }
   }
 
+  async putSale(accountId, saleId, sale: PostSale): Promise<Sale> {
+    const url = `https://api.lightspeedapp.com/API/Account/${accountId}/Sale/${saleId}.json`;
+
+    const options = {
+      method: 'PUT',
+      url,
+      data: sale,
+    };
+
+    try {
+      const response = await this.performRequest(options);
+      return response.data.Sale as Sale;
+    } catch (err) {
+      return this.handleResponseError('PUT SALE', err);
+    }
+  }
+  
   async putItem(accountId, item, ID) {
     const url = `https://api.lightspeedapp.com/API/Account/${accountId}/Item/${ID}.json`;
 
